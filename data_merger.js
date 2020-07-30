5d6a556138743ebb51350ba98ec15fd204e24e9b
Marco Pagani <marco.pagani@ufl.edu>
2020-07-30 11:26:52 -0400

    collected all google books data

--- data_merger.js
+++ data_merger.js
@@ -1,15 +1,19 @@
 let fs = require('fs')
-let google_data = require('./books.json')
-let lib_data = require('./database.json')
-
-google_data = google_data.books
+let google_data = require('./formatted_data/google_books.json')
+let lib_data = require('./formatted_data/database_books.json')
 
 let site_data = []
-google_data.forEach(function (book, i) {
-    if (!book.error) {
-
-    } else {
-            console.log(lib_data[i].Title)
-            console.log(lib_data[i].Author)
-    }
+google_data.forEach(function (gbook, i) {
+    let lbook = lib_data[i]
+    let title = gbook.title
+    if (gbook.subtitle) { title = title + ': ' + gbook.subtitle }
+    /*
+    site_data.push({
+        'title': title,
+        'authors': gbook.authors
+    })*/
+    console.log(title)
+    console.log(lbook.Title)
+    console.log()
+    
 })
--- data_scraper.js
+++ data_scraper.js
@@ -3,22 +3,19 @@ const fs = require('fs')
 const axios = require('axios');
 const rateLimit = require('axios-rate-limit');
 
-
+const database = require('./formatted_data/database_books.json')
 const http = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 2000 })
 
-const database = require('./database.json')
-const results = require('./books.json')
+let promises = []
 let output = {
     books: []
 }
-let promises = []
 
-results.books.forEach(function (item, i) {
-    if (item.error) {
-        let book = database[i]
+function searchISBN() {
+    database.forEach(function (book, i) {
         let ISBN = book.ISBN.toString().split(' ')[0]
         let title = book.Title.replace(/[^a-zA-Z ]/g, "").replace(/\s/g, "+").toLowerCase()
-        let url = 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + title
+        let url = 'https://www.googleapis.com/books/v1/volumes?q=ibsn:' + ISBN
         promises.push(http.get(url)
             .then(function (response) {
                 if (response.data.totalItems > 0) {
@@ -26,9 +23,9 @@ results.books.forEach(function (item, i) {
                     output.books.push(response.data.items[0].volumeInfo)
                 }
                 else {
-                    console.log('not found')
+                    console.log('not found, retrying')
                     output.books.push({
-                        error: book.title
+                        error: title
                     })
                 }
             }).catch(
@@ -40,43 +37,47 @@ results.books.forEach(function (item, i) {
             )
         )
     }
-})
+    )
+};
 
-Promise.all(promises).then(result => {
-    fs.writeFile('more_books.json', JSON.stringify(output), 'utf8', results => {
-        console.log('done')
-    });
-})
-
-
-
-
-
-
-/*
-fs.createReadStream('database_short.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-    .on('end', () => {
-        results.forEach(book => {
-            //console.log(book)
-
-            axios.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + book.ISBN
-              )
+function searchTitle() {
+    database.forEach(function (book, i) {
+        if (book.error) {
+            let title = book.error
+            let url = 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + title
+            promises.push(http.get(url)
                 .then(function (response) {
-                  console.log(response)
-              })
+                    if (response.data.totalItems > 0) {
+                        console.log('got')
+                        output.books.push(response.data.items[0].volumeInfo)
+                    }
+                    else {
+                        console.log('not found')
+                        output.books.push({
+                            error: title
+                        })
+                    }
+                }).catch(
+                    function (error) {
+                        console.log('failed ')
+                        output.books.push({ error: 'api request failed' })
+                        return Promise.resolve()
+                    }
+                )
+            )
+        }
+    }
+    )
+};
 
-            let grub = book.Title.replace(/[^A-Z0-9]+/ig, "_");
-            if (grub.length > 15) {
-                grub = grub.substr(0,35)
-                grub = grub.substr(0, grub.lastIndexOf("_")).toLowerCase()
-            }
-            if (!fs.existsSync('out/' + grub)){
-                fs.mkdirSync('out/' + grub);
-            }
-
-
-        })
-
-  });*/
+searchISBN();
+Promise.all(promises)
+    .then(result => {
+        searchTitle();
+        Promise.all(promises)
+            .then(result => {
+                fs.writeFile('results.json', JSON.stringify(output), 'utf8', results => {
+                    console.log('done')
+                })
+            })
+    })
